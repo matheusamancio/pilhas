b199a470781c7a967b642684e6e6e35fee5bda67
Matheus Amancio Seixeiro <matheusamancio23@hotmail.com>
2015-02-06 10:38:59 -0200

    Initial Commit

---
+++ Pilha1.1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pilha1.1
+//
+//  Created by Matheus Amancio Seixeiro on 06/02/15.
+//  Copyright (c) 2015 Matheus Amancio Seixeiro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
